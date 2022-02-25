045c823375f3d8c0bcbc6d2b96147617ee663408
Josh Tillinghast <jwtill@icloud.com>
2022-02-25 14:38:29 -0800

    Pass if Count is 2 or less for Pastries

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Bakery.Models
 {
   public class Pastry //Class def
@@ -10,6 +11,30 @@ namespace Bakery.Models
     {
       Cost = 0;
       Count = count;
+    }
+    public int calculateCost() 
+    { 
+      double cost = 0;
+      double decCount = Convert.ToDouble(Count);
+      if(Count <= 2) 
+      {
+        cost += 2 * Count;
+      }
+      // } else if(Count % 3 == 0) 
+      // {
+      //   cost = Count * 3.33;
+      // } else if(Count % 3 == 1) 
+      // {
+      //   cost = Count * 3.75;
+      // } else if(Count % 3 == 2) 
+      // {
+      //   cost = Count * 4;
+      // } 
+      else 
+      {
+        return 0;
+      }
+      return Convert.ToInt32(cost);
     }
   }
 }
