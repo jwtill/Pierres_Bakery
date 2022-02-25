4d374c1dd531409de6ae8b8f5e5d846ac47a89ae
Josh Tillinghast <jwtill@icloud.com>
2022-02-25 15:01:30 -0800

    Update system.cs to use pastry class and calculate and print order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,13 +9,17 @@ namespace Bakery
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre\'s Bakery! \nBread costs $5 each or 3 for $10!\nPastries cost $2 each or 3 for $5!");
+
       Console.WriteLine("How many loaves of bread would you like?");
       int breadCount = int.Parse(Console.ReadLine());
       Console.WriteLine("How many pastries would you like?");
       int pastryCount = int.Parse(Console.ReadLine());
 
       Bread newBreadOrder = new Bread(breadCount);
-      Console.WriteLine("Your total of " + breadCount + " loaves is " + "$" + newBreadOrder.calculateCost() + ".");
+      Console.WriteLine("Your " + breadCount + " bread loaves come to " + "$" + newBreadOrder.calculateCost() + ".");
+      
+      Pastry newPastryOrder = new Pastry(pastryCount);
+      Console.WriteLine("Your " + pastryCount + " pastries come to " + "$" + newPastryOrder.calculateCost() + ".");
     }
   }
 }
