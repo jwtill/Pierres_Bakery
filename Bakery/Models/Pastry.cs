bb35d358accc7aef2f9868a2236a017e82510687
Josh Tillinghast <jwtill@icloud.com>
2022-03-01 19:30:28 -0800

    Pass new pastry tests

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -14,29 +14,18 @@ namespace Bakery.Models
 
     public int calculateCost() 
     { 
-      double cost = 0;
-      double decCount = Convert.ToDouble(Count);
-      if(decCount <= 2) 
-      {
-        cost += 2 * decCount;
+      int cost = 0;
+      for(int i = 1; i <= Count; i++) {
+        if (i % 3 == 0) 
+        {
+          cost += 1;
+        } else 
+        {
+          cost += 2;
+        }
+        
       }
-      else if(decCount % 3 == 0) 
-      {
-        cost = decCount * 1.666;
-      }
-       else if(decCount % 3 == 1) 
-      {
-        cost = decCount * 1.75;
-      } 
-      else if(decCount % 3 == 2) 
-      {
-        cost = decCount * 1.8;
-      } 
-      else 
-      {
-        return 0;
-      }
-      return Convert.ToInt32(cost);
+      return cost;
     }
   }
 }
